5af32bd4cbbec96776bda480c6a296a132690635
GuillermoCamino <92096380+GuillermoCamino@users.noreply.github.com>
2022-10-28 11:10:36 +0200

    ejercicios restantes

--- 03_arrays/ejercicios/ejercicio4.php
+++ 03_arrays/ejercicios/ejercicio4.php
@@ -4,38 +4,89 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../styles.css">
+    <title>Ejercicio 4</title>
 </head>
 <body>
-    
-
+    <h1>Ejercicio 4</h1>
     <?php
-
-$series = [
-    [" edgeRunner", "Netflix", 1],
-    ["la casa del dragon", "HBO", 1],
-    [" the bigbang theory", "Netflix", 10],
-    [" Vikingos ", "HBO", 6],
-    ["andor", "Disney+", 1],
-    ["Dora la exploradora ", " Disney +", 3],
+    $series = [
+        ["El Vigilante", "Netflix", 2],
+        ["Vikings", "HBO", 6],
+        ["Spy x Family", "Netflix", 2],
+        ["Andor", "Disney Plus", 1],
+        ["Dora la Exploradora", "Disney Plus", 3]
     ];
     ?>
-    <table border=1px>
+
+    <table>
         <tr>
-            <th>Titulo</th>
+            <th>Título</th>
             <th>Plataforma</th>
             <th>Temporadas</th>
         </tr>
         <?php
-        foreach ($series as $serie){
-            list ($titulo,$plataforma,$temporadas) = $serie;
-        
+        foreach ($series as $serie) {
+            list($titulo, $plataforma, $temporadas) = $serie;
         ?>
+            <tr>
+                <td><?php echo $titulo ?></td>
+                <td><?php echo $plataforma ?></td>
+                <td><?php echo $temporadas ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+
+    <br><br>
+
+    <h2>Tabla ordenada por temporadas</h2>
+
+    <table>
         <tr>
-            <td><?php echo $titulo?></td>
-            <td><?php echo $plataforma?></td>
-            <td><?php echo $temporadas?></td>
+            <th>Título</th>
+            <th>Plataforma</th>
+            <th>Temporadas</th>
         </tr>
+        <?php
+        $temporadas = array_column($series, 2);
+        array_multisort($temporadas, SORT_DESC, $series);
+        foreach ($series as $serie) {
+            list($titulo, $plataforma, $temporadas) = $serie;
+        ?>
+            <tr>
+                <td><?php echo $titulo ?></td>
+                <td><?php echo $plataforma ?></td>
+                <td><?php echo $temporadas ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+
+    <br><br>
+
+    <h2>Tabla ordenada por plataforma y título</h2>
+
+    <table>
+        <tr>
+            <th>Título</th>
+            <th>Plataforma</th>
+            <th>Temporadas</th>
+        </tr>
+        <?php
+        $titulo = array_column($series, 0);
+        $plataforma = array_column($series, 1);
+        array_multisort($plataforma, SORT_ASC, $titulo, SORT_ASC, $series);
+        foreach ($series as $serie) {
+            list($titulo, $plataforma, $temporadas) = $serie;
+        ?>
+            <tr>
+                <td><?php echo $titulo ?></td>
+                <td><?php echo $plataforma ?></td>
+                <td><?php echo $temporadas ?></td>
+            </tr>
         <?php
         }
         ?>
