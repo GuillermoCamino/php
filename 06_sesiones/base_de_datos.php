e931da4d56992ef005b70c3e8b3e0ced3e074e60
GuillermoCamino <92096380+GuillermoCamino@users.noreply.github.com>
2022-11-15 11:14:53 +0100

    login y registro

---
+++ 06_sesiones/base_de_datos.php
@@ -0,0 +1,10 @@
+<?php
+    $servidor= 'localhost';
+    $usuario='admin';
+    $contrasena = 'Adminbd26';
+    $base_de_datos='db_usuarios';
+
+
+    $conexion= new MySQLi($servidor,$usuario,$contrasena,$base_de_datos) or die("Erros en al conexion");
+
+?>
