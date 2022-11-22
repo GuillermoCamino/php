7160118a7401dade465ae576861cdf81dd4887e2
GuillermoCamino <92096380+GuillermoCamino@users.noreply.github.com>
2022-11-22 11:54:19 +0100

    laravel principio

---
+++ 07_laravel/videojuegos/app/Http/Controllers/VideojuegosController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VideojuegosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mensaje="Esta es la lista de Videojuegos";
+        $videojuegos=[
+            ["pokemon diamante", 40, 3,"juego remaster de pokemon ds"],
+            ["god of war", 70, 18,"juego que va de matar dioses"],
+            ["the binding of isaac", 20, 16,"Juego que va de ir por mazmorras"],
+            ["League of legends ", 0, 15,"Juego que va de ser racista"],
+            ["need for speed", 80, 12,"brum brum"],
+            ["pokemon escarlata",60,3,"simulador de bugs pero juegazo en vd"],
+        ];
+        return view('videojuegos/index',[
+            'mensaje' => $mensaje,
+            'videojuegos' => $videojuegos
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('videojuegos/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
